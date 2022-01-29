d3dd47d3573b1dbed5999046971c9bcb9e091724
myrul deen <funstudy10@gmail.com>
2022-01-30 00:39:12 +0800

    add graph option

--- backend-test/routes/data.js
+++ backend-test/routes/data.js
@@ -2,27 +2,67 @@ const express = require('express');
 const Data = require('../model/data.model');
 const router = express.Router();
 
+router.get('/graph/:deviceId', async function (req, res) {
+    var macAddress = req.params.deviceId;
+
+    let today = new Date();
+    today.setHours(0, 0, 0, 0)
+    let first = today.getDate() - today.getDay();
+    let last = first + 6;
+    let firstday = new Date(today.setDate(first)).toUTCString();
+    let lastday = new Date(today.setDate(last)).toUTCString();
+    let firstDayMonth = new Date(today.setDate(1));
+    let lastDayMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0)
+    lastDayMonth.setHours(23, 59, 59, 0);
+    today = new Date().setHours(0, 0, 0, 0);
+
+    let d = await Promise.all([
+        Data.find({
+            macAddress: macAddress,
+            createdAt: {
+                $gte: today
+            }
+        }).exec(),
+        Data.find({
+            macAddress: macAddress,
+            createdAt: {
+                $gte: firstday,
+                $lte: lastday
+            }
+        }).exec(),
+        Data.find({
+            macAddress: macAddress,
+            createdAt: {
+                $gte: firstDayMonth,
+                $lte: lastDayMonth
+            }
+        }).exec()
+    ]);
+    res.json(d);
+});
+
 router.get('/:deviceId/:limit', function (req, res) {
     var macAddress = req.params.deviceId;
     var limit = parseInt(req.params.limit) || 30;
     Data
-    .find({
-        macAddress: macAddress
-    })
-    .sort({'createdAt': -1})
-    .limit(limit)
-    .exec(function(err, devices) {
-        if (err) return res.status(500).send(err);
-        res.status(200).json(devices);
-    });
+        .find({
+            macAddress: macAddress
+        })
+        .sort({ 'createdAt': -1 })
+        .limit(limit)
+        .exec(function (err, devices) {
+            if (err) return res.status(500).send(err);
+            res.status(200).json(devices);
+        });
 });
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
     var data = req.body;
     // data.createdBy = req.user._id;
-    Data.create(data, function(err, _data) {
+    Data.create(data, function (err, _data) {
         if (err) return res.status(500).send(err);
         res.json(_data);
     });
-})
+});
+
 module.exports = router;
--- project-name/src/pages/view-device/view-device.ts
+++ project-name/src/pages/view-device/view-device.ts
@@ -21,11 +21,12 @@ export class ViewDevicePage {
 
 
   public device: any;
-	public data: Array<any>;
-	public toggleState: boolean = false;
-	private subData: any;
-	public lastRecord: any;
+  public data: Array<any>;
+  public toggleState: boolean = false;
+  private subData: any;
+  public lastRecord: any;
   isData: boolean = false;
+  pvalue: any = "day";
 
   // temp: any = 0;
   // Line Chart
@@ -69,7 +70,7 @@ export class ViewDevicePage {
   public lineChartType: string = 'line';
 
   public lineChartData: Array<any> = [];
-  public lineChartLabels:Array<any> = [];
+  public lineChartLabels: Array<any> = [];
   // public labels: Array<any> = ;
 
   public lineChartColors: Array<any> = [
@@ -115,139 +116,145 @@ export class ViewDevicePage {
     private deviceService: DeviceService,
     private toastService: ToastService) {
 
-      this.device = navParams.get("device");
+    this.device = navParams.get("device");
 
   }
 
   ionViewDidLoad() {
     this.deviceService.getOne(this.device._id).subscribe((response) => {
-			this.device = response.json();
-			this.getData();
-			this.socketInit();
-		});
+      this.device = response.json();
+      this.getData();
+      this.socketInit();
+    });
+    console.log(this.pvalue);
     // this.socketInit();
   }
 
   getData() {
-		this.dataService.get(this.device.macAddress).subscribe((response) => {
-			this.data = response.json();
-			this.genChart();
-			this.lastRecord = this.data[0]; // descending order data
-			if (this.lastRecord) {
-				this.toggleState = this.lastRecord.data.l;
-			}
-		});
-	}
+    this.dataService.get(this.device.macAddress).subscribe((response) => {
+      this.data = response.json();
+      this.genChart();
+      this.lastRecord = this.data[0]; // descending order data
+      if (this.lastRecord) {
+        this.toggleState = this.lastRecord.data.l;
+      }
+    });
+  }
 
 
   socketInit() {
-		this.subData = this.socket.getDataV2(this.device.macAddress).subscribe((data) => {
-			if (this.data.length <= 0) return;
-			this.data.splice(this.data.length - 1, 1); // remove the last record
-			this.data.push(data); // add the new one
-			this.lastRecord = data;
-		}, (err) => console.error(err));
-	}
+    this.subData = this.socket.getDataV2(this.device.macAddress).subscribe((data) => {
+      if (this.data.length <= 0) return;
+      this.data.splice(this.data.length - 1, 1); // remove the last record
+      this.data.push(data); // add the new one
+      this.lastRecord = data;
+    }, (err) => console.error(err));
+  }
 
   ionViewDidUnload() {
-		this.subData ? this.subData.unsubscribe() : '';
-	}
+    this.subData ? this.subData.unsubscribe() : '';
+  }
 
   genChart() {
-		let data = this.data;
-		let _dtArr: Array<any>  = [];
-		let _lblArr: Array<any> = [];
+    let data = this.data;
+    let _dtArr: Array<any> = [];
+    let _lblArr: Array<any> = [];
 
-    let lm35: Array<any>   = [];
-    let light: Array<any>  = [];
-    let volt: Array<any>   = [];
-    let amp: Array<any>    = [];
+    let lm35: Array<any> = [];
+    let light: Array<any> = [];
+    let volt: Array<any> = [];
+    let amp: Array<any> = [];
     let tmpArr: Array<any> = [];
-		let humArr: Array<any> = [];
+    let humArr: Array<any> = [];
 
-		for (var i = 0; i < data.length; i++) {
-			let _d = data[i];
+    for (var i = 0; i < data.length; i++) {
+      let _d = data[i];
       lm35.push(_d.data.LM35);
       light.push(_d.data.light);
       volt.push(_d.data.voltage);
       amp.push(_d.data.current);
-			tmpArr.push(_d.data.temp);
-			humArr.push(_d.data.humd);
-			_lblArr.push(this.formatDate(_d.createdAt));
-		}
-		// reverse data to show the latest on the right side
+      tmpArr.push(_d.data.temp);
+      humArr.push(_d.data.humd);
+      _lblArr.push(this.formatDate(_d.createdAt));
+    }
+    // reverse data to show the latest on the right side
     lm35.reverse();
     light.reverse();
     volt.reverse();
     amp.reverse();
-		tmpArr.reverse();
-		humArr.reverse();
-		_lblArr.reverse();
-		_dtArr = [
+    tmpArr.reverse();
+    humArr.reverse();
+    _lblArr.reverse();
+    _dtArr = [
       {
-				data: lm35,
-				label: 'LM35',
+        data: lm35,
+        label: 'LM35',
         fill: false,
         // borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
-			},
+      },
       {
-				data: light,
-				label: 'Light',
+        data: light,
+        label: 'Light',
         fill: false,
         // borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
-			},
+      },
       {
-				data: volt,
-				label: 'Voltage',
+        data: volt,
+        label: 'Voltage',
         fill: false,
         // borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
-			},
+      },
       {
-				data: amp,
-				label: 'Current',
+        data: amp,
+        label: 'Current',
         fill: false,
         // borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
-			},
-			{
-				data: tmpArr,
-				label: 'Temperature',
+      },
+      {
+        data: tmpArr,
+        label: 'Temperature',
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
-			},
-			{
-				data: humArr,
-				label: 'Humidity',
+      },
+      {
+        data: humArr,
+        label: 'Humidity',
         fill: false,
         borderColor: 'rgb(85, 165, 192)',
         tension: 0.1
-			}
-		];
-		
-		// this.lineChartData = _dtArr.slice(0, 10);
-		// this.lineChartLabels = _lblArr.slice(0, 10);
-		this.lineChartData = _dtArr;
-		this.lineChartLabels = _lblArr;
-		this.isData = true;
-	}
+      }
+    ];
+
+    // this.lineChartData = _dtArr.slice(0, 10);
+    // this.lineChartLabels = _lblArr.slice(0, 10);
+    this.lineChartData = _dtArr;
+    this.lineChartLabels = _lblArr;
+    this.isData = true;
+  }
 
   getLatest() {
-		
-		this.getData();
-    this.genChart();
-      this.toastService.toggleToast('Graph updated');
 
-	}
+    this.getData();
+    this.genChart();
+    this.toastService.toggleToast('Graph updated');
+
+  }
 
   private formatDate(originalTime) {
-		var d = new Date(originalTime);
-		var datestring =
-			d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes();
-		return datestring;
-	}
+    var d = new Date(originalTime);
+    var datestring =
+      d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes();
+    return datestring;
+  }
+
+  getValue() {
+    console.log(this.pvalue);
+    // the value will be displayed
+  }
 
 }
